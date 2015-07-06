106247e4872963a8044211f5d1af7e86b4589d12
flavioambrosi <flavio.ambrosi@gmail.com>
2015-07-05 23:01:31 -0300

    Atualizacoes

---
+++ entrega-mercadorias-web/src/main/java/org/logistica/servicos/ServicoBuscaMenorCaminho.java
@@ -0,0 +1,33 @@
+package org.logistica.servicos;
+
+import java.math.BigDecimal;
+
+import javax.ejb.EJB;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import org.logistica.ServicosEntregaMercadorias;
+import org.logistica.exception.VerticeNotFoundExcetion;
+
+@WebService (serviceName="ServicoBuscaMenorCaminho/servicoBuscaMenorCaminho")
+public class ServicoBuscaMenorCaminho {
+
+	@EJB
+	ServicosEntregaMercadorias servicos;
+	
+	@WebMethod
+	public void buscaCaminho(@WebParam(name = "origem") String origem,
+			@WebParam(name = "destino") String destino,
+			@WebParam(name = "distancia")Integer distancia,
+			@WebParam(name = "autonomia") BigDecimal autonomia,
+			@WebParam(name = "valorCombustivel") BigDecimal valorCombustivel) {
+				
+			try {
+				servicos.buscaCaminho(origem, destino, autonomia, valorCombustivel);
+			} catch (VerticeNotFoundExcetion e) {
+				e.printStackTrace();
+			}
+		
+	}
+}
